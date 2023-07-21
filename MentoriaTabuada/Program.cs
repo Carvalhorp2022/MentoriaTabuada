a7dca6867c8cc238b80d5da532cf2711d00d7840
Rodrigo Carvalho <carvalhorp@taginfraestrutura.com.br>
2023-07-21 11:59:51 -0300

    Initial commit

---
+++ MentoriaTabuada/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+int multiplicar;
+int multiplicador;
+
+Console.Write("Digite um numero: ");
+
+multiplicar = Convert.ToInt32(Console.ReadLine());
+
+for (multiplicador = 0; multiplicador <= 10; multiplicador++)
+{
+   Console.WriteLine(string.Format("{0}*{1}={2}", multiplicar, multiplicador, multiplicar*multiplicador)); 
+}
+
+Console.ReadKey();
